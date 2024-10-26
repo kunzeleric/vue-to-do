c899f08c96577199eedd6dd03138c2068f59c417
Eric Kunzel <kunzel.eric@gmail.com>
2024-10-26 10:58:33 -0300

    feat: created header component

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import logo from '@/assets/images/logo.png'
+</script>
+
+<template>
+  <header class="w-full bg-gray-900 flex justify-center items-center py-4 h-52">
+    <img v-bind:src="logo" alt="" class="w-40 h-16" />
+  </header>
+</template>
