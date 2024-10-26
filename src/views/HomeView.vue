a463ece3630f7880c8d4f1f3995efa8de5474cb8
Eric Kunzel <kunzel.eric@gmail.com>
2024-10-26 10:58:42 -0300

    feat: created homeview

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Input from '@/components/Input.vue'
+import Tasks from '@/components/Tasks.vue'
+</script>
+
+<template>
+  <main class="bg-zinc-900 h-[calc(100vh-13rem)]">
+    <div
+      class="-translate-y-7 flex flex-col gap-20 justify-center items-center w-[732px] mx-auto"
+    >
+      <Input />
+      <Tasks />
+    </div>
+  </main>
+</template>
