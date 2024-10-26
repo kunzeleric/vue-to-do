ae5006ab655f38d6b08b373dc5cdc593f7faea76
Eric Kunzel <kunzel.eric@gmail.com>
2024-10-26 10:59:05 -0300

    feat: created input and task components

---
+++ src/components/Input.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { PhPlusCircle } from '@phosphor-icons/vue'
+</script>
+
+<template>
+  <form class="flex gap-2 h-16 w-full">
+    <div class="h-full w-full">
+      <input
+        placeholder="Adicione uma nova tarefa"
+        class="w-full h-full bg-gray-700 border-black border-2 outline-none shadow-none rounded-md px-4 text-white"
+        type="text"
+      />
+    </div>
+    <button
+      class="flex gap-2 bg-sky-600 font-bold rounded-md px-4 text-white items-center"
+      type="submit"
+    >
+      Criar
+      <PhPlusCircle size="20" weight="bold" />
+    </button>
+  </form>
+</template>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import clipboard from '@/assets/images/clipboard.png'
+</script>
+
+<template>
+  <section class="w-full">
+    <div class="flex justify-between">
+      <div class="flex gap-2 items-center">
+        <p class="text-sky-600 font-bold">Tarefas Criadas</p>
+        <span class="rounded-full text-white bg-gray-700 font-bold px-2 py-0.5"
+          >0</span
+        >
+      </div>
+      <div class="flex gap-2 items-center">
+        <p class="text-purple-600 font-bold">Concluídas</p>
+        <span class="rounded-full text-white bg-gray-700 font-bold px-2 py-0.5"
+          >0</span
+        >
+      </div>
+    </div>
+
+    <!-- Placeholder for no tasks registered -->
+    <div
+      class="flex justify-center items-center border-t border-gray-400 rounded-t-md mt-4 flex-col"
+    >
+      <div class="flex flex-col pt-12 items-center gap-4">
+        <img v-bind:src="clipboard" alt="" class="w-16 h-16" />
+        <div class="flex flex-col justify-center items-center">
+          <p class="font-bold text-gray-400">
+            Você ainda não tem tarefas cadastradas
+          </p>
+          <p class="text-gray-400">
+            Crie tarefas e organize seus itens a fazer
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
